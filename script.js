e1266ddf61ad6119fdc1c352d0c02d1b0ef9610f
Eduardo <duduferreiraf551@gmail.com>
2026-02-03 18:56:23 -0300

    base do projeto

---
+++ script.js
@@ -0,0 +1,15 @@
+function filtrar(categoria, botao) {
+    const itens = document.querySelectorAll('.item');
+    const botoes = document.querySelectorAll('.filtros button');
+
+    botoes.forEach(btn => btn.classList.remove('ativo'));
+    botao.classList.add('ativo');
+
+    itens.forEach(item => {
+        if (categoria === 'todos' || item.classList.contains(categoria)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
